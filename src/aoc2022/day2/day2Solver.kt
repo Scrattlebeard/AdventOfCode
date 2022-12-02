abc698bccb5bb012a9b859d553b06f380d2a6acf
Scrattlebeard <juhl.daniel@gmail.com>
2022-12-02 07:23:03 +0100

    Day 2 prep

---
+++ src/aoc2022/day2/day2Solver.kt
@@ -0,0 +1,25 @@
+package aoc2022.day2
+
+import lib.*
+
+suspend fun main() {
+    challenge<List<String>> {
+
+        day(2)
+        year(2022)
+
+        parser {
+            it.readLines()
+        }
+
+        solver {
+            println("Solving challenge 1...")
+            "Todo challenge 1"
+        }
+
+        solver {
+            println("Solving challenge 2...")
+            "Todo challenge 2"
+        }
+    }
+}
